a27ea870748424021a7cefba0b5d9a57e111b93c
louarnos <louarnos@gmail.com>
2016-06-22 12:25:14 -0400

    Changed URL in app-data for deployment

--- assets/app-data.js
+++ assets/app-data.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  // api : 'https://pacific-refuge-81971.herokuapp.com'
-  api: 'http://localhost:3000'
+  api : 'https://pacific-refuge-81971.herokuapp.com'
+  // api: 'http://localhost:3000'
 };
 
 
